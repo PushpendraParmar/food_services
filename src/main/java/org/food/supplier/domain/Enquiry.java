1bfed24b2b605cc2e6d3927b86a53e6472e3edbf
Narendra Singh <narendrasikarwar04@gmail.com>
2018-03-26 22:47:27 +0530

    Added Enquiry Web services. Added Enquiry Entity. Added Enquiry DAO Added Enquiry Service.

--- src/main/java/org/food/supplier/domain/Enquiry.java
+++ src/main/java/org/food/supplier/domain/Enquiry.java
@@ -7,8 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//@Entity
-//@Table(name="enquiry")
+@Entity
+@Table(name="enquiry")
 public class Enquiry {
 	
 	@Id
